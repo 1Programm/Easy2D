7c52e1ebb7d355fb270b67b2d9f1c7dfd0f748cb
Programm0 <1Pro2gra3mm45@web.de>
2021-12-05 12:11:12 +0100

    Adding basic engine api interfaces

---
+++ easy2d-api/src/main/java/com/programm/project/easy2d/api/IEngine.java
@@ -0,0 +1,9 @@
+package com.programm.project.easy2d.api;
+
+public interface IEngine {
+
+    void update();
+
+    void render(IPencil pencil);
+
+}
---
+++ easy2d-api/src/main/java/com/programm/project/easy2d/api/IPencil.java
@@ -0,0 +1,20 @@
+package com.programm.project.easy2d.api;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
+public interface IPencil {
+
+    void setColor(Color c);
+
+    void drawPixel(float x, float y);
+
+    void drawLine(float x1, float y1, float x2, float y2);
+
+    void drawRectangle(float x, float y, float width, float height);
+
+    void fillRectangle(float x, float y, float width, float height);
+
+    void drawImage(BufferedImage img, float x, float y, float width, float height);
+
+}
